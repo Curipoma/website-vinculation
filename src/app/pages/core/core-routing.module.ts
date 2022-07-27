b1a998e389f59af8d15712bf2e3c5301d51a0493
Curipoma <at.curipoma@yavirac.edu.ec>
2022-07-27 07:24:38 -0500

    nav-general-coordinator

--- src/app/pages/core/core-routing.module.ts
+++ src/app/pages/core/core-routing.module.ts
@@ -3,37 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'careers',
-    // loadChildren: () => import('./career/career.module').then(m => m.CareerModule)
+    path: 'general-coordinators',
+    loadChildren: () =>
+      import('./general-coordinator/general-coordinator.module').then(
+        (m) => m.GeneralCoordinatorModule
+      ),
   },
-  {
-    path: 'catalogues',
-    // loadChildren: () => import('./catalogue/catalogue.module').then(m => m.CatalogueModule)
-  },
-  {
-    path: 'curricula',
-    // loadChildren: () => import('./curriculum/curriculum.module').then(m => m.CurriculumModule)
-  },
-  {
-    path: 'information-students',
-    // loadChildren: () => import('./information-student/information-student.module').then(m => m.InformationStudentModule)
-  },
-  {
-    path: 'information-teachers',
-    // loadChildren: () => import('./information-teacher/information-teacher.module').then(m => m.InformationTeacherModule)
-  },
-  {
-    path: 'institutions',
-    // loadChildren: () => import('./institution/institution.module').then(m => m.InstitutionModule)
-  },
-  {
-    path: 'students',
-    // loadChildren: () => import('./student/student.module').then(m => m.StudentModule)
-  },
-  {
-    path: 'subjects',
-    // loadChildren: () => import('./subject/subject.module').then(m => m.SubjectModule)
-  },
+
 ];
 
 @NgModule({
---
+++ src/app/pages/core/general-coordinator/projects/general-data/general-data.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-general-data',
+  templateUrl: './general-data.component.html',
+  styleUrls: ['./general-data.component.scss']
+})
+export class GeneralDataComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
